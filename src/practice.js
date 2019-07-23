9d32f1bbef98ed0e36a277a24821b568b91e8130
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-23 11:51:34 -0400

    created script for create.shopping-list and seed

---
+++ src/practice.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+knexInstance('whopipe_video_views')
+  .select('*') 
+  .then(res => console.log(res))
+  .catch(err => console.log(err.message))
+  .finally(() => knexInstance.destroy());
